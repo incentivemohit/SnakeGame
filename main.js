3a74d131e9c695a05198a1bca518dced222921b2
mkcoder597 <86621804+incentivemohit@users.noreply.github.com>
2023-01-22 23:02:21 +0530

    first

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var boxsize = 25;
-var rows = 20;
+var rows = 18;
 var cols = 20;
 var context;
 var board;
@@ -15,8 +15,7 @@ var velocityY = 0;
 
 var snake = [];
 var gameOver = false;
-var count = 0;
-var snakeLength = 0;
+var score = 25;
 
 window.onload = function () {
   board = document.getElementById("board");
@@ -24,7 +23,11 @@ window.onload = function () {
   board.height = rows * boxsize;
   context = board.getContext("2d");
   placeFood();
-  document.addEventListener("keyup", changeDirection);
+  document.getElementById("leftButton").addEventListener("click", moveLeft);
+  document.getElementById("upButton").addEventListener("click", moveUp);
+  document.getElementById("rightButton").addEventListener("click", moveRight);
+  document.getElementById("downButton").addEventListener("click", moveDown);
+
   setInterval(update, 1000 / 10);
 };
 
@@ -37,13 +40,13 @@ function update() {
   context.fillStyle = "black";
   context.fillRect(0, 0, board.width, board.height);
 
-  context.fillStyle = "green";
+  context.fillStyle = "yellow";
   context.fillRect(foodX, foodY, boxsize, boxsize);
 
   if (snakeX == foodX && snakeY == foodY) {
     snake.push([foodX, foodY]);
-    snakeLength++;
-    document.getElementById("game-score").innerHTML = snakeLength;
+    score = score + 25;
+    document.getElementById("game-score").innerHTML = score;
     placeFood();
   }
 
@@ -54,7 +57,7 @@ function update() {
     snake[0] = [snakeX, snakeY];
   }
 
-  context.fillStyle = "red";
+  context.fillStyle = "white";
   snakeX += velocityX * boxsize;
   snakeY += velocityY * boxsize;
   context.fillRect(snakeX, snakeY, boxsize, boxsize);
@@ -69,36 +72,53 @@ function update() {
     snakeY < 0 ||
     snakeY > (rows - 1) * boxsize
   ) {
-    document.getElementById("game-over").style.display = "block";
+    document.getElementById("game-body").style.display = "none";
+    document.getElementById("game-over-indicator1").style.display = "block";
     document.getElementById("play-button").onclick = function () {
       gameOver = true;
     };
   }
   for (let i = 0; i < snake.length; i++) {
     if (snakeX == snake[i][0] && snakeY == snake[i][1]) {
-      gameOver = true;
-      alert("Game Over! Play Again");
+      document.getElementById("game-body").style.display = "none";
+      document.getElementById("game-over-indicator2").style.display = "block";
+      document.getElementById("play-button").onclick = function () {
+        gameOver = true;
+      };
     }
-  }
-}
-
-function changeDirection(e) {
-  if (e.code == "ArrowLeft" && velocityX != 1) {
-    velocityX = -1;
-    velocityY = 0;
-  } else if (e.code == "ArrowUp" && velocityY != 1) {
-    velocityX = 0;
-    velocityY = -1;
-  } else if (e.code == "ArrowRight" && velocityX != -1) {
-    velocityX = 1;
-    velocityY = 0;
-  } else if (e.code == "ArrowDown" && velocityY != -1) {
-    velocityX = 0;
-    velocityY = 1;
   }
 }
 
 function placeFood() {
   foodX = Math.floor(Math.random() * cols) * boxsize;
   foodY = Math.floor(Math.random() * rows) * boxsize;
+}
+
+//game control functions
+
+function moveLeft() {
+  if (velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  }
+}
+
+function moveUp() {
+  if (velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  }
+}
+
+function moveRight() {
+  if (velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
+function moveDown() {
+  if (velocityX != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  }
 }
